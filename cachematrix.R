d429a1f592c6883343e544cfc7c1b617194e1435
skrishn40 <skrishn40@gmail.com>
2018-03-24 13:29:23 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,28 +4,31 @@
 ## Get and Set functions set the source matrix and local variables
 
 makeCacheMatrix <- function(x = matrix()) {
-  inv <- matrix()
-  set <- function(y) {
+  inv <- NULL      # set inv variable to NULL
+  set <- function(y) {     # set value of matrix in parent environment
     x <<- y
-    inv <<- matrix()
+    inv <<- NULL
   }
-  get <- function() x
-  setinverse <- function(mat) inv <<- mat
-  getinverse <- function() inv
+  get <- function() x      # get value of matrix argument
+  setinverse <- function(inverse) inv <<- inverse  # sets the parent variable to inverse argument
+  getinverse <- function() inv                    # gets the inverse value
   list(set = set, get = get,
        setinverse = setinverse,
        getinverse = getinverse)
 }
 
-## cacheSolve uses cached data if available otherwise it uses solve function to get the inverse of argument
-cacheSolve <- function(x, ...) {
-  inv1 <- x$getinverse()
-  if(!is.na(inv1)) {
+# Function will accept special "Vector" of type makeCacheVector as argument
+# it will provide cache value if already available otherwise it will solve the inverse
+# and store in cache for future retrieval
+
+cacheSolve <- function(x, ...) {  # X = special vector of type makeCacheMatrix
+  inverse <- x$getinverse()       # get cache value if available
+  if(!is.null(inverse)) {         # if not null, use it
     message("getting cached data")
-    return(inv1)
+    return(inverse)
   }
-  data <- x$get()
-  inv1 <- solve(data, ...)
-  x$setinverse(inv1)
-  inv1
+  data <- x$get()                 
+  inverse <- solve(data, ...)     # otherwise, use Solve to compute the Inverse matrix
+  x$setinverse(inverse)           # .. and store it in the "cache"/parent env. variable
+  inverse                               # and also return the inverse
 }
